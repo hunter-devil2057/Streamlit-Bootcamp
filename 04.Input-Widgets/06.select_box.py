4c3926bddf9e634baa9b8180e0c7272671e9e01e
Sheewakoti Manish <87434771+hunter-devil2057@users.noreply.github.com>
2025-05-26 11:29:20 +0545

    Select Box in Streamlit

---
+++ 04.Input-Widgets/06.select_box.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+option = st.selectbox(
+    "How would you like to be contacted???", 
+    ("Email", "Home Phone", "Mobile Phone")
+)
+
+st.write(f"You Selected: {option}")
